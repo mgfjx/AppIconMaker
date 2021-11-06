2272c1108c5c055aab3312c8212c51bbf4901ca3
mgfjx <mgfjxxiexiaolong@gmail.com>
2021-11-06 22:56:30 +0800

    init

---
+++ AppIconHelper/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AppIconHelper
+//
+//  Created by mgfjx on 2021/11/6.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
